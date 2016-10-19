60ac24c856d1502c9e5280830029744d0182622b
aceparis32 <aceparis32@gmail.com>
2016-10-19 19:59:46 +0700

    Mengubah Homework.js

--- homework.js
+++ homework.js
@@ -1,6 +1,6 @@
 // Mary Rose's JavaScript Homework
 
-// 1. Write an if/else statement
+// 1. Tulis perintah if dan else
 var APPLE_QUANTITY = 5;
 
 if (APPLE_QUANTITY < 5){
@@ -9,17 +9,17 @@ if (APPLE_QUANTITY < 5){
   console.log("You have 5 or more apples. Hurrah!");
 }
 
-// 2. Write a "for" loop
+// 2. Tulis "for" untuk proses looping
 for(i = 0; i < APPLE_QUANTITY; i ++){
   console.log("WEEEEEEE! I'm looping!")
 }
 
-// 3. Write a function
+// 3. tulis fungsi
 var sayHello = function(){
   console.log("Hello!")
 }
 
-// 4. Create an object
+// 4. Tulis untuk membuat objek
 var mary = {
   name: "Mary Rose",
   codeNinja: true,
